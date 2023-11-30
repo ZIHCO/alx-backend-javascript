499ccb0dad3646281ce155376c5acfcf0d1c6e63
ZIHCO <chizobajames21@gmail.com>
2023-11-30 17:42:29 +0100

    lint fixes

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,8 +2,6 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  //const keys = ['getIncomeInDollars', 'getIncomeInEuros']
-  //const values = [`$${income}`, `${income} euros`]
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
