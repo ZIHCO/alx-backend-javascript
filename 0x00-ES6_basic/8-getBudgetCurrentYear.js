1ba6eed6f3bbb9291eb659a73ccda40ee670cf83
ZIHCO <chizobajames21@gmail.com>
2023-11-30 17:20:05 +0100

    lint fixes

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,12 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const keys = [`income-${getCurrentYear()}`, `gdp-${getCurrentYear()}`, `capita-${getCurrentYear()}`]
-  const values = [income, gdp, capita]
+  const keys = [`income-${getCurrentYear()}`, `gdp-${getCurrentYear()}`, `capita-${getCurrentYear()}`];
+  const values = [income, gdp, capita];
   const budget = keys.reduce((result, key, index) => {
-    result[key] = values[index];
-    return result;
+    const res = result;
+    res[key] = values[index];
+    return res;
   }, {});
 
   return budget;
