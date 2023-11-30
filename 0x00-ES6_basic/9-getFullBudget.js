f7d635d7e1b705e8d45f506b026567e63e4341e5
ZIHCO <chizobajames21@gmail.com>
2023-11-30 12:09:28 +0100

    method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  //const keys = ['getIncomeInDollars', 'getIncomeInEuros']
+  //const values = [`$${income}`, `${income} euros`]
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
