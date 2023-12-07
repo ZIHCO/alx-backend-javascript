029cd955c2900cf449dcbf3b400208bbe8d177d3
ZIHCO <chizobajames21@gmail.com>
2023-12-07 18:41:29 +0100

    fixes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
 export function getResponseFromAPI() {
-  const myPromise = new Promise(((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const x = 0;
     if (x === 0) {
       resolve('OK');
     } else {
       reject('Error');
     }
-  }));
-  return myPromise;
+  });
 }
