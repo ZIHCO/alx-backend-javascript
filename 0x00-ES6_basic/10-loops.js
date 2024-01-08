7032a3fe840fa4b9f01c7401564b72258d260c29
ZIHCO <chizobajames21@gmail.com>
2024-01-08 03:04:59 +0100

    for...of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = array;
   for (const item of array) {
-    const test = (value, index, arr) => value === item;
+    const test = (value) => value === item;
     const idx = arr.findIndex(test);
     arr[idx] = appendString + item;
   }
