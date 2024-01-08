16570bd265d3a7a86628b42024ebf796e38c16d4
ZIHCO <chizobajames21@gmail.com>
2024-01-08 04:00:41 +0100

    delete old file

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,9 +0,0 @@
-export default function createIteratorObject(report) {
-  let allEmployees = [];
-  for (const property in report.allEmployees) {
-    if (property) {
-      allEmployees = [...allEmployees, ...report.allEmployees[property]];
-    }
-  }
-  return allEmployees;
-}
