b1c137bf670ab1984e2933d753a5424e329ce314
zihco <chizobajames21@gmail.com>
2024-02-27 22:10:23 +0100

    sync readFile

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,33 +4,24 @@ module.exports = function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  /* const fileLines = fs
+  const fileLines = fs
     .readFileSync(path, 'utf8')
     .toString('utf8')
     .trim()
-    .split('\n'); */
-  fs.readFileSync(path, 'utf8', (data) => {
-    console.log(data);
-    let sliceList = [];
-    const studentsField = {};
-    const list = data.split('\n');
-    if (list[list.length - 1] === '') {
-      sliceList = list.slice(1, (list.length - 1));
-    } else {
-      sliceList = list.slice();
-    }
-    console.log(`Number of students: ${sliceList.length}`);
-    for (const item of sliceList) {
-      const listItem = item.split(',');
-      studentsField[listItem[3]] = [];
-    }
-    for (const item of sliceList) {
-      const listItem = item.split(',');
-      studentsField[listItem[3]].push(listItem[0]);
-    }
-    for (const key of Object.keys(studentsField)) {
-      const strList = studentsField[key].join(', ');
-      console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
-    }
-  });
+    .split('\n')
+    .slice(1);
+  const studentsField = {};
+  console.log(`Number of students: ${fileLines.length}`);
+  for (const item of fileLines) {
+    const listItem = item.split(',');
+    studentsField[listItem[3]] = [];
+  }
+  for (const item of fileLines) {
+    const listItem = item.split(',');
+    studentsField[listItem[3]].push(listItem[0]);
+  }
+  for (const key of Object.keys(studentsField)) {
+    const strList = studentsField[key].join(', ');
+    console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
+  }
 };
