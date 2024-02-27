8bd3b35add46e1e6d5a13b3acd11fa77ddfa4317
zihco <chizobajames21@gmail.com>
2024-02-27 22:31:06 +0100

    done

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    let sliceList = [];
+    const studentsField = {};
+    if (err) {
+      throw new Error('Cannot load the database');
+    } else {
+      const list = data.split('\n');
+      if (list[list.length - 1] === '') {
+        sliceList = list.slice(1, (list.length - 1));
+      } else {
+        sliceList = list.slice();
+      }
+      console.log(`Number of students: ${sliceList.length}`);
+      for (const item of sliceList) {
+        const listItem = item.split(',');
+        studentsField[listItem[3]] = [];
+      }
+      for (const item of sliceList) {
+        const listItem = item.split(',');
+        studentsField[listItem[3]].push(listItem[0]);
+      }
+      for (const key of Object.keys(studentsField)) {
+        const strList = studentsField[key].join(', ');
+        console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
+      }
+    }
+  });
+};
