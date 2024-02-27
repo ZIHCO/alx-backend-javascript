ee848a419dc385cf60e8e9fe698f5240db9061e4
zihco <chizobajames21@gmail.com>
2024-02-27 21:49:26 +0100

    sync readFile

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,36 +4,33 @@ module.exports = function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  const fileLines = fs
+  /* const fileLines = fs
     .readFileSync(path, 'utf8')
     .toString('utf8')
     .trim()
-    .split('\n');
-  /*fs.readFile(path, 'utf8', (err, data) => {
+    .split('\n'); */
+  fs.readFileSync(path, 'utf8', (data) => {
+    console.log(data);
     let sliceList = [];
     const studentsField = {};
-    if (err) {
-      throw new Error('Cannot load the database');
+    const list = data.split('\n');
+    if (list[list.length - 1] === '') {
+      sliceList = list.slice(1, (list.length - 1));
     } else {
-      const list = data.split('\n');
-      if (list[list.length - 1] === '') {
-        sliceList = list.slice(1, (list.length - 1));
-      } else {
-        sliceList = list.slice();
-      }
-      console.log(`Number of students: ${sliceList.length}`);
-      for (const item of sliceList) {
-        const listItem = item.split(',');
-        studentsField[listItem[3]] = [];
-      }
-      for (const item of sliceList) {
-        const listItem = item.split(',');
-        studentsField[listItem[3]].push(listItem[0]);
-      }
-      for (const key of Object.keys(studentsField)) {
-        const strList = studentsField[key].join(', ');
-        console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
-      }
+      sliceList = list.slice();
     }
-  });*/
+    console.log(`Number of students: ${sliceList.length}`);
+    for (const item of sliceList) {
+      const listItem = item.split(',');
+      studentsField[listItem[3]] = [];
+    }
+    for (const item of sliceList) {
+      const listItem = item.split(',');
+      studentsField[listItem[3]].push(listItem[0]);
+    }
+    for (const key of Object.keys(studentsField)) {
+      const strList = studentsField[key].join(', ');
+      console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
+    }
+  });
 };
