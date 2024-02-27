ce307f6afecf51094884058c1fbb5a31b78d4f85
zihco <chizobajames21@gmail.com>
2024-02-27 23:34:24 +0100

    done

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,30 @@
 const fs = require('fs');
 
-module.exports = function countStudents(path) {
-  if (!fs.exists(path)) {
-    throw new Error('Cannot load the database');
-  }
-  /* fs.readFile(path, 'utf8', (err, data) => {
-    let sliceList = [];
-    const studentsField = {};
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      const list = data.split('\n');
-      if (list[list.length - 1] === '') {
-        sliceList = list.slice(1, (list.length - 1));
-      } else {
-        sliceList = list.slice();
-      }
-      console.log(`Number of students: ${sliceList.length}`);
-      for (const item of sliceList) {
-        const listItem = item.split(',');
-        studentsField[listItem[3]] = [];
-      }
-      for (const item of sliceList) {
-        const listItem = item.split(',');
-        studentsField[listItem[3]].push(listItem[0]);
-      }
-      for (const key of Object.keys(studentsField)) {
-        const strList = studentsField[key].join(', ');
-        console.log(`Number of students in CS: ${studentsField[key].length}. List: ${strList}`);
-      }
+      reject(new Error('Cannot load the database'));
     }
-  }); */
-};
+    const fileLines = data
+      .trim()
+      .split('\n')
+      .slice(1);
+    const studentsField = {};
+    console.log(`Number of students: ${fileLines.length}`);
+    for (const item of fileLines) {
+      const listItem = item.split(',');
+      studentsField[listItem[3]] = [];
+    }
+    for (const item of fileLines) {
+      const listItem = item.split(',');
+      studentsField[listItem[3]].push(listItem[0]);
+    }
+    for (const key of Object.keys(studentsField)) {
+      const strList = studentsField[key].join(', ');
+      console.log(`Number of students in ${key}: ${studentsField[key].length}. List: ${strList}`);
+    }
+    resolve(true);
+  });
+});
+
+module.exports = countStudents;
