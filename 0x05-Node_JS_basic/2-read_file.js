26854338657ac0a84cbd2e4bca7cc177ace63fae
zihco <chizobajames21@gmail.com>
2024-02-27 13:49:32 +0100

    reading file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 module.exports = function countStudents(path) {
   fs.readFile(path, 'utf8', (err, data) => {
     let sliceList = [];
+    const studentsField = {};
     if (err) {
       throw new Error('Cannot load the database');
     } else {
@@ -13,14 +14,13 @@ module.exports = function countStudents(path) {
         sliceList = list.slice();
       }
       console.log(`Number of students: ${sliceList.length}`);
-      const studentsField = { CS: [], SWE: [] };
       for (const item of sliceList) {
         const listItem = item.split(',');
-        if (listItem[3] === 'CS') {
-          studentsField.CS.push(listItem[0]);
-        } else {
-          studentsField.SWE.push(listItem[0]);
-        }
+        studentsField[listItem[3]] = [];
+      }
+      for (const item of sliceList) {
+        const listItem = item.split(',');
+        studentsField[listItem[3]].push(listItem[0]);
       }
       console.log(`Number of students in CS: ${studentsField.CS.length}. List: ${studentsField.CS}`);
       console.log(`Number of students in SWE: ${studentsField.SWE.length}. List: ${studentsField.SWE}`);
