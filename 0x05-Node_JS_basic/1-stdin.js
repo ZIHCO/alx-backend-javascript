72768748763ce9801267b2a654840c61f5605496
zihco <chizobajames21@gmail.com>
2024-02-27 09:15:31 +0100

    child_process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    process.stdout.write('Your name is: ' + input);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
