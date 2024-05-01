c99d124b72b098592dc17ada509bc20f6c2fd655
zihco <chizobajames21@gmail.com>
2024-05-01 23:00:42 +0100

    http server and promises

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('This is the list of our students'));
+    } else {
+      const arrOfStudents = data
+        .trim()
+        .split('\n')
+        .slice(1);
+      const arrOfReport = [];
+      arrOfReport.push('This is the list of our students');
+      arrOfReport.push(`Number of students: ${arrOfStudents.length}`);
+      const studentsObject = {};
+      for (const student of arrOfStudents) {
+        studentsObject[student.split(',')[3]] = [];
+      }
+      for (const student of arrOfStudents) {
+        studentsObject[student.split(',')[3]].push(student.split(',')[0]);
+      }
+      for (const key of Object.keys(studentsObject)) {
+        arrOfReport.push(`Number of students in ${key}: ${studentsObject[key].length}. List: ${studentsObject[key].join(', ')}`);
+      }
+      resolve(arrOfReport.join('\n'));
+    }
+  });
+});
+
+const PORT = 1245;
+const HOST = '127.0.0.1';
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((report) => {
+        res.end(report);
+      })
+      .catch((err) => {
+        res.end(err);
+      });
+  }
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running at ${HOST}:${PORT}/`);
+});
+
+module.exports = app;
