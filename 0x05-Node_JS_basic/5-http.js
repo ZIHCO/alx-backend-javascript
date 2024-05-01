ea163851397fc4aa9c72dc514d2403d96756aaa6
zihco <chizobajames21@gmail.com>
2024-05-01 23:03:46 +0100

    http server and promises

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const countStudents = (path) => new Promise((resolve, reject) => {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      reject(new Error('This is the list of our students'));
+      reject(new Error('Cannot load the database'));
     } else {
       const arrOfStudents = data
         .trim()
