4a75c411a9eac6323d277ccc19ea0e6c209626c7
zihco <chizobajames21@gmail.com>
2024-05-01 04:55:40 +0100

    promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 
 const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.open(path, 'r', (err) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+  });
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      reject(new Error('Cannot load the database'));
+      reject(err);
     }
-    const fileLines = data
+    const arrOfStudents = data
       .trim()
       .split('\n')
       .slice(1);
-    const studentsField = {};
-    console.log(`Number of students: ${fileLines.length}`);
-    for (const item of fileLines) {
-      const listItem = item.split(',');
-      studentsField[listItem[3]] = [];
+    console.log(`Number of students: ${arrOfStudents.length}`);
+    const fieldObjects = {};
+    for (const student of arrOfStudents) {
+      fieldObjects[student.split(',')[3]] = [];
     }
-    for (const item of fileLines) {
-      const listItem = item.split(',');
-      studentsField[listItem[3]].push(listItem[0]);
+    for (const student of arrOfStudents) {
+      fieldObjects[student.split(',')[3]].push(student.split(',')[0]);
     }
-    for (const key of Object.keys(studentsField)) {
-      const strList = studentsField[key].join(', ');
-      console.log(`Number of students in ${key}: ${studentsField[key].length}. List: ${strList}`);
+    for (const key of Object.keys(fieldObjects)) {
+      console.log(`Number of students in ${key}: ${fieldObjects[key].length}. ${fieldObjects[key].join(', ')}`);
     }
     resolve(true);
   });
