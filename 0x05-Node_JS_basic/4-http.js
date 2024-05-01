ac655ff6b991309c7deb947e81093cfb25a82db4
zihco <chizobajames21@gmail.com>
2024-05-01 09:16:50 +0100

    server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const { createServer } = require('node:http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
