f7a93c25fa4863a72ffa5e0f7c214d376b6a15be
zihco <chizobajames21@gmail.com>
2024-05-02 04:38:45 +0100

    fixed 3-read...

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,21 +4,23 @@ const countStudents = (path) => new Promise((resolve, reject) => {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
       reject(new Error('Cannot load the database'));
-    }
-    const arrOfStudents = data
-      .trim()
-      .split('\n')
-      .slice(1);
-    console.log(`Number of students: ${arrOfStudents.length}`);
-    const fieldObjects = {};
-    for (const student of arrOfStudents) {
-      fieldObjects[student.split(',')[3]] = [];
-    }
-    for (const student of arrOfStudents) {
-      fieldObjects[student.split(',')[3]].push(student.split(',')[0]);
-    }
-    for (const key of Object.keys(fieldObjects)) {
-      console.log(`Number of students in ${key}: ${fieldObjects[key].length}. List: ${fieldObjects[key].join(', ')}`);
+    } else {
+      const arrOfStudents = data
+        .trim()
+        .split('\n')
+        .slice(1);
+      console.log(`Number of students: ${arrOfStudents.length}`);
+      const fieldObjects = {};
+      for (const student of arrOfStudents) {
+        fieldObjects[student.split(',')[3]] = [];
+      }
+      for (const student of arrOfStudents) {
+        fieldObjects[student.split(',')[3]].push(student.split(',')[0]);
+      }
+      for (const key of Object.keys(fieldObjects)) {
+        console.log(`Number of students in ${key}: ${fieldObjects[key].length}. List: ${fieldObjects[key].join(', ')}`);
+        resolve(true);
+      }
     }
   });
 });
