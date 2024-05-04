4d16e3c81cda6dc5de42684a29c28b45a216d1aa
zihco <chizobajames21@gmail.com>
2024-05-04 03:31:00 +0100

    more test suite add

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,4 +10,6 @@ describe('calculateNumber', function () {
   it('correctly round and adds 2 numbers', test({ a: 8.4, b: 4.7, expected: 13 }));
   it('a is rounded', test({ a: 8.4, b: 1, expected: 9}));
   it('b is rounded', test({ a: 8, b: 1.9, expected: 10}));
+  it('a is rounded', test({ a: 6.5, b: 11.3, expected: 18}));
+  it('a is rounded', test({ a: 8.6, b: 81.5, expected: 91}));
 });
