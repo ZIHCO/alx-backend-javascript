e0b0ada4393a53f621ef269659460c988623b31a
zihco <chizobajames21@gmail.com>
2024-05-05 05:58:40 +0100

    done

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const calculateNumber = require('./2-calcul.js');
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', function () {
   const test = ({ operator, a, b, expected }) =>
