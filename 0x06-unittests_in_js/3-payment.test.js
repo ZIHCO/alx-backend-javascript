697a10cf35e35334201c85f361006e1743363404
zihco <chizobajames21@gmail.com>
2024-05-06 04:51:07 +0100

    done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
 const Utils = require('./utils.js');
-const spy = require('sinon').spy;
+const sinon = require('sinon');
 const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', function () {
   it('Validates the usage of the Utils function', function () {
-    const spyThis = spy(Utils);
+    const spyThis = sinon.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spyThis.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     expect(spyThis.calculateNumber.callCount).to.equal(1);
