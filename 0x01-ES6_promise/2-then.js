63270e2a1f5ed3f97dc15e1591d09b19fe3ea115
zihco <chizobajames21@gmail.com>
2024-05-07 07:59:41 +0100

    done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API'))
 }
 
 export default handleResponseFromAPI;
