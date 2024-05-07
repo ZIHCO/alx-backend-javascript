cfa9f8da5e95385470d2b572dd8e37c663113c39
zihco <chizobajames21@gmail.com>
2024-05-07 11:13:45 +0100

    resolved promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 function signUpUser(firstName, lastName) {
-  return (new Promise((resolve) => resolve({'firstName': firstName, 'lastName': lastName})));
+  return (new Promise((resolve) => resolve({ firstName, lastName })));
 }
 
 export default signUpUser;
