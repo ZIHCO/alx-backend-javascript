0ce3a6375f15d4a0cb0ddb56d57f2a74bad6b714
zihco <chizobajames21@gmail.com>
2024-05-23 07:52:21 +0100

    allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
-    .then((results) => {
-      // const arrOfObjects = [];
-      results.forEach((result) => console.log(result));
-    });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => console.log(results));
 }
 
 export default handleProfileSignup;
