740330d8308fe826077c846374f98fdf456da240
zihco <chizobajames21@gmail.com>
2024-05-24 09:30:06 +0100

    all done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,27 +1,21 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function () {
-  const arrOfPromises = await Promise.allSettled([
+  const arrOfPromises = await Promise.all([
     uploadPhoto(), createUser(),
   ]).then((value) => value).catch((err) => err);
 
-  return arrOfPromises.reduce((obj, entry, index) => {
-    try {
-      if (Object.hasOwnProperty.call(entry, 'reason')) {
-        throw new Error(entry.reason);
-      }
+  try {
+    return arrOfPromises.reduce((obj, entry, index) => {
       if (index === 0) {
-        return { ...obj, photo: entry.value };
+        return { ...obj, photo: entry };
       }
       if (index === 1) {
-        return { ...obj, user: entry.value };
-      }
-    } catch (err) {
-      if (err) {
-        return obj;
+        return { ...obj, user: entry };
       }
       return obj;
-    }
-    return obj;
-  }, { photo: null, user: null });
+    }, {});
+  } catch () {
+    return {photo: null, user: null};
+  }
 }
