fb50f12040242914ed225c77db041ed718bfb495
zihco <chizobajames21@gmail.com>
2024-05-27 21:14:43 +0100

    done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,29 +1,21 @@
 export default class Building {
   constructor(sqft) {
-    try {
-      if (this.constructor !== Building) {
-        if (this.evacuationWarningMessage === undefined) {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
       }
-      /*if (typeof sqft !== 'number') {
-        throw TypeError('Sqft must be a number');
-      }*/
-      this._sqft = sqft;
-    } catch (err) {
-      console.error(err.toString());
     }
+    if (typeof sqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 
   set sqft(value) {
-    try {
-      /*if (typeof value !== 'number') {
-        throw TypeError('Sqft must be a number');
-      }*/
-      this._sqft = value;
-    } catch (err) {
-      console.error(err);
+    if (typeof value !== 'number') {
+      throw TypeError('Sqft must be a number');
     }
+    this._sqft = value;
   }
 
   get sqft() {
