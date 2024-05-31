18b455228f873c369ad38ba5f687a465902e0ea4
zihco <chizobajames21@gmail.com>
2024-05-31 11:14:15 +0100

    learning symbol

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new Error('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new Error('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+  }
+}
