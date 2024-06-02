2e7da70fc696ecfd219ea9d9f49c28980b2d229c
zihco <chizobajames21@gmail.com>
2024-06-02 07:07:47 +0100

    subset

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,12 @@
 export default function hasValuesFromArray(set, arr) {
-  return ((new Set(arr)).isSubsetOf(set));
+  let bool;
+  arr.forEach((entry) => {
+    if (!set.has(entry)) {
+      bool = 0;
+      return false;
+    }
+    bool = 1;
+    return true;
+  });
+  return (!!bool);
 }
