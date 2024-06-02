e29325827f63b496d1c26d4375a6c7d001611874
zihco <chizobajames21@gmail.com>
2024-06-02 09:17:05 +0100

    join, slice,

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
   set.forEach((entry) => {
-    if (startString.length && entry.slice(0, startString.length) === startString) {
+    if ((entry && startString.length) && entry.slice(0, startString.length) === startString) {
       result.push(entry.slice(startString.length));
     }
     return true;
