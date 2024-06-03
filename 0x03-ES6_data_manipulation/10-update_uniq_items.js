280cf9f7138dddd41fc1760a53592cd7fc915bfb
zihco <chizobajames21@gmail.com>
2024-06-03 07:27:30 +0100

    map object

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
 export default function updateUniqueItems(map) {
   map.forEach((value, key, thisMap) => {
-    try {
-      if (thisMap.get(key) === 1) {
-        thisMap.set(key, 100);
-      }
-    } catch (err) {
+    if (map.toString() !== '[object Map]') {
       throw new Error('Cannot process');
+    }
+    if (thisMap.get(key) === 1) {
+      thisMap.set(key, 100);
     }
   });
   return map;
