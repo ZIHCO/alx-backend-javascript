0dcb01157763a98dfb47f0af6d6c36dbcb684e0c
zihco <chizobajames21@gmail.com>
2024-06-04 23:17:00 +0100

    done

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -9,28 +9,29 @@ app.get('/students', (req, res) => {
   fs.readFile(process.argv[2], 'utf8', (err, data) => {
     if (err) {
       res.send('Cannot load the database');
-    } else {
-      const arrOfData = data
-        .trim()
-        .split('\n')
-        .slice(1);
-
-      const responseString = (`This is the list of our students\nNumber of students: ${arrOfData.length}`);
-
-      const groupByField = arrOfData.reduce((res, entry) => {
-        const entryArray = entry.split(',');
-        res[entryArray[3]].push(entryArray[0]);
-        return res;
-      }, { CS: [], SWE: [] });
-
-      const analytics = Object
-        .keys(groupByField)
-        .reduce((arr, entry) => {
-          arr.push(`\nNumber of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`);
-          return arr;
-        }, []);
-      res.send(responseString.concat(analytics.join('')));
+      return 'Failed!';
     }
+    const arrOfData = data
+      .trim()
+      .split('\n')
+      .slice(1);
+
+    const responseString = (`This is the list of our students\nNumber of students: ${arrOfData.length}`);
+
+    const groupByField = arrOfData.reduce((res, entry) => {
+      const entryArray = entry.split(',');
+      res[entryArray[3]].push(entryArray[0]);
+      return res;
+    }, { CS: [], SWE: [] });
+
+    const analytics = Object
+      .keys(groupByField)
+      .reduce((arr, entry) => {
+        arr.push(`\nNumber of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`);
+        return arr;
+      }, []);
+    res.send(responseString.concat(analytics.join('')));
+    return 'Successful!';
   });
 });
 
