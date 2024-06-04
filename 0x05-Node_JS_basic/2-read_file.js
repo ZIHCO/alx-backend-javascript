968a37e78527d678a0b2fe81210392a5fedff1aa
zihco <chizobajames21@gmail.com>
2024-06-04 09:28:06 +0100

    done

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,31 +2,20 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
+  const arrOfData = fs.readFileSync(path, 'utf8')
+    .trim()
+    .split('\n')
+    .slice(1);
 
-    const arrOfData = data
-      .toString()
-      .trim()
-      .split('\n')
-      .slice(1);
+  console.log(`Number of students: ${arrOfData.length}`);
 
-    console.log(`Number of students: ${arrOfData.length}`);
+  const groupByField = arrOfData.reduce((res, entry) => {
+    const entryArray = entry.split(',');
+    res[entryArray[3]].push(entryArray[0]);
+    return res;
+  }, { CS: [], SWE: [] });
 
-    const groupByField = arrOfData.reduce((res, entry) => {
-      const entryArray = entry.split(',');
-      res[entryArray[3]].push(entryArray[0]);
-      return res;
-    }, { CS: [], SWE: [] });
-
-    Object
-      .keys(groupByField)
-      .forEach(
-        (entry) => console.log(`Number of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`),
-      );
-  });
+  Object.keys(groupByField).forEach(
+    (entry) => console.log(`Number of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`),
+  );
 }
-
-module.exports = countStudents;
