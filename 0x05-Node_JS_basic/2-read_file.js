d7e9205dd7641d987a4651d534932f48010832f7
zihco <chizobajames21@gmail.com>
2024-06-04 09:49:01 +0100

    done

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,15 +7,15 @@ function countStudents(path) {
       .trim()
       .split('\n')
       .slice(1);
-  
+
     console.log(`Number of students: ${arrOfData.length}`);
-  
+
     const groupByField = arrOfData.reduce((res, entry) => {
       const entryArray = entry.split(',');
       res[entryArray[3]].push(entryArray[0]);
       return res;
     }, { CS: [], SWE: [] });
-  
+
     Object.keys(groupByField).forEach(
       (entry) => console.log(`Number of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`),
     );
