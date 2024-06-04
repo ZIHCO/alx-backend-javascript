610da7c7e47a296d87df8ed0cdeee72178207a67
zihco <chizobajames21@gmail.com>
2024-06-04 22:50:43 +0100

    done

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,37 +1,15 @@
 const express = require('express');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
 app.get('/students', (req, res) => {
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-      res.send('Cannot load the database');
-    }
-
-    const arrOfData = data
-      .trim()
-      .split('\n')
-      .slice(1);
-
-    let responseString = (`This is the list of our students\nNumber of students: ${arrOfData.length}`);
-
-    const groupByField = arrOfData.reduce((res, entry) => {
-      const entryArray = entry.split(',');
-      res[entryArray[3]].push(entryArray[0]);
-      return res;
-    }, { CS: [], SWE: [] });
-
-    const analytics = Object
-      .keys(groupByField)
-      .reduce((arr, entry) => {
-        arr.push(`\nNumber of students in ${entry}: ${groupByField[entry].length}. List: ${groupByField[entry].join(', ')}`);
-        return arr;
-      }, []);
-    res.send(responseString.concat(analytics.join('')));
-  });
+  const newPromise = countStudents(process.argv[2]);
+  newPromise
+    .then((res) => res)
+    .catch((err) => err);
 });
 
 app.listen(1245);
