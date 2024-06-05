d5bcb22c51fb8253faeb2a2aec244966b1d15673
zihco <chizobajames21@gmail.com>
2024-06-05 05:12:01 +0100

    done

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -14,15 +14,15 @@ const app = http.createServer((request, response) => {
         .trim()
         .split('\n')
         .slice(1);
-  
+
       const responseString = (`This is the list of our students\nNumber of students: ${arrOfData.length}`);
-  
+
       const groupByField = arrOfData.reduce((res, entry) => {
         const entryArray = entry.split(',');
         res[entryArray[3]].push(entryArray[0]);
         return res;
       }, { CS: [], SWE: [] });
-  
+
       const analytics = Object
         .keys(groupByField)
         .reduce((arr, entry) => {
