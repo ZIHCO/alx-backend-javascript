3805aa639fc8f25fc81c016fad1d22123f47c16c
zihco <chizobajames21@gmail.com>
2024-06-06 03:59:32 +0100

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,11 +8,11 @@ describe('calculateNumber', () => {
       assert.strictEqual(res, expected);
     };
   it('It should test if the return is the expected value', test({ a: 5.5, b: 4.9, expected: 11 }));
-  it('It should test if it works for integers', test({ a: 5, b: 5, expected: 10 }));
-  it('It should test if it works for 2 negatives', test({ a: -7.1, b: -7.6, expected: -15 }));
-  it('It should test if it works for mixed type', test({ a: -7.1, b: 0, expected: -7}));
+  it('a is rounded', test({ a: 5, b: 5, expected: 10 }));
+  it('b is rounded', test({ a: -7.1, b: -7.6, expected: -15 }));
+  /*it('It should test if it works for mixed type', test({ a: -7.1, b: 0, expected: -7}));
   it('It should test if it works for mixed type', test({ a: -7.1, b: 7, expected: 0 }));
   it('It should test if the return is of type INT', () => {
     assert.strictEqual(Number.isInteger(calculateNumber(5.5, 4.9)), true);
-  });
+  });*/
 });
