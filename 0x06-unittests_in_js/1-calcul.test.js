72779651268411eb7679f68da07ad842ec1e2184
zihco <chizobajames21@gmail.com>
2024-06-06 23:30:26 +0100

    done

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -8,13 +8,14 @@ describe('calculateNumber', () => {
       assert.strictEqual(res, expected);
     };
 
-  /* Testcases for division */
-  it('correctly round and adds 2 numbers', test({ type: 'DIVIDE', a: 5.5, b: 4.9, expected: 1.2 }));
-  it('a is rounded', test({ type: 'DIVIDE', a: 5.9, b: 5, expected: 1.2 }));
-  it('b is rounded', test({ type: 'DIVIDE', a: -7, b: -7.4, expected: 1 }));
-  it('zero divisor', test({ type: 'DIVIDE', a: -7.1, b: 0.9, expected: -7}));
-  it('It should test if it works for mixed type', test({ type: 'DIVIDE', a: 16.4, b: 7.9, expected: 2 }));
-  it('test if it throws an Error', () => {
-    assert.throws(() => calculateNumber('DIVIDE', 5.5, 0.4), Error, 'Error');
+  describe('Testcases for division', () => {
+    it('correctly round and adds 2 numbers', test({ type: 'DIVIDE', a: 5.5, b: 4.9, expected: 1.2 }));
+    it('a is rounded', test({ type: 'DIVIDE', a: 5.9, b: 5, expected: 1.2 }));
+    it('b is rounded', test({ type: 'DIVIDE', a: -7, b: -7.4, expected: 1 }));
+    it('zero divisor', test({ type: 'DIVIDE', a: -7.1, b: 0.9, expected: -7}));
+    it('It should test if it works for mixed type', test({ type: 'DIVIDE', a: 16.4, b: 7.9, expected: 2 }));
+    it('test if it throws an Error', () => {
+      assert.throws(() => calculateNumber('DIVIDE', 5.5, 0.4), Error, 'Error');
+    });
   });
 });
