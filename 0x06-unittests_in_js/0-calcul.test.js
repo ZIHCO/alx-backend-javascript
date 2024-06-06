2ab0715b8da761491fd1e0c543684ff4a6094835
zihco <chizobajames21@gmail.com>
2024-06-06 03:36:48 +0100

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,21 +3,21 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('It should test if the return is the expected value', () => {
-    assert.equal(calculateNumber(5.5, 4.9), 11);
+    assert.strictEqual(calculateNumber(5.5, 4.9), 11);
   });
   it('It should test if the return is of type INT', () => {
-    assert.equal(Number.isInteger(calculateNumber(5.5, 4.9)), true);
+    assert.strictEqual(Number.isInteger(calculateNumber(5.5, 4.9)), true);
   });
   it('It should test if it works for integers', () => {
-    assert.equal(calculateNumber(5, 5), 10);
+    assert.strictEqual(calculateNumber(5, 5), 10);
   });
   it('It should test if it works for 2 negatives', () => {
-    assert.equal(calculateNumber(-7.1, -7.6), -15);
+    assert.strictEqual(calculateNumber(-7.1, -7.6), -15);
   });
   it('It should test if it works for mixed type', () => {
-    assert.equal(calculateNumber(-7.1, 0), -7);
+    assert.strictEqual(calculateNumber(-7.1, 0), -7);
   });
   it('It should test if it works for mixed type', () => {
-    assert.equal(calculateNumber(-7.1, 7), 0);
+    assert.strictEqual(calculateNumber(-7.1, 7), 0);
   });
 });
