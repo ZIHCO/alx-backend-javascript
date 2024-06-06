7f8aa64405c37746c189b9b2caab83c663de79a7
zihco <chizobajames21@gmail.com>
2024-06-06 04:13:00 +0100

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,15 +1,18 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
+describe('calculateNumber', () => {
   const test = ({ a, b, expected }) =>
     function () {
       const res = calculateNumber(a, b);
       assert.strictEqual(res, expected);
     };
-  it('correctly round and adds 2 numbers', test({ a: 8.4, b: 4.7, expected: 13 }));
-  it('a is rounded', test({ a: 8.4, b: 1, expected: 9}));
-  it('b is rounded', test({ a: 8, b: 1.9, expected: 10}));
-  /*it('a is rounded', test({ a: 6.5, b: 11.3, expected: 18}));
-  it('a is rounded', test({ a: 8.6, b: 81.5, expected: 91}));*/
+  it('the return value is correctly rounded', test({ a: 5.5, b: 4.9, expected: 11 }));
+  it('a is rounded', test({ a: 5, b: 5, expected: 10 }));
+  it('b is rounded', test({ a: -7.1, b: -7.6, expected: -15 }));
+  /*it('It should test if it works for mixed type', test({ a: -7.1, b: 0, expected: -7}));
+  it('It should test if it works for mixed type', test({ a: -7.1, b: 7, expected: 0 }));
+  it('It should test if the return is of type INT', () => {
+    assert.strictEqual(Number.isInteger(calculateNumber(5.5, 4.9)), true);
+  });*/
 });
