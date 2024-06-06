e78cda6bd4282303f1ed2aedb8544ae298ed3028
zihco <chizobajames21@gmail.com>
2024-06-06 23:22:11 +0100

    done

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -10,8 +10,7 @@ describe('calculateNumber', () => {
 
   /* Testcase for subtract */
   it('correctly round and adds 2 numbers', test({ type: 'SUBTRACT', a: 5.5, b: 4.9, expected: 1 }));
-  it('a is rounded', test({ type: 'SUBTRACT', a: 5.9, b: 5, expected: 1 }));
-  /*it('b is rounded', test({ type: 'SUBTRACT', a: -7, b: -7.6, expected: 1 }));*/
+  /*it('a is rounded', test({ type: 'SUBTRACT', a: 5.9, b: 5, expected: 1 }));*/
 
   /* Testcases for division */
   it('correctly round and adds 2 numbers', test({ type: 'DIVIDE', a: 5.5, b: 4.9, expected: 1.2 }));
