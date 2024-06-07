3087ffe2052d1dd1e521453fd76f629d6c90f233
zihco <chizobajames21@gmail.com>
2024-06-07 05:16:42 +0100

    done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,7 +4,12 @@ const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
-  const callback = spy(Utils);
-  sendPaymentRequestToApi(100, 20);
-  expect(callback.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  it('Validates the usage of Utils module in sendPaymentRequestAPI', () => {
+    const callback = spy(Utils);
+    sendPaymentRequestToApi(100, 20);
+    expect(callback.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(callback.calculateNumber.callCount).to.equal(1);
+    callback..calculateNumber.restore();
+  });
 });
+
