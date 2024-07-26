b71eb2419d4b59a0524684c82eef2a8243c00b7b
zihco <chizobajames21@gmail.com>
2024-07-26 05:40:51 +0100

    done

--- 0x06-unittests_in_js/4-payment.js
+++
@@ -1,8 +0,0 @@
-const Utils = require('./utils.js');
-
-function sendPaymentRequestToApi (totalAmount, totalShipping) {
-  const grandTotal = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${grandTotal}`);
-}
-
-module.exports = sendPaymentRequestToApi;
